81f4459d2237d6bcbf9cdbb4ef895276ab09f73f
Gustavo <gustavolovizotto@gmail.com>
2024-10-25 09:45:53 -0300

    Created the files of the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Hello} from './hello';
+
+function App() {
+  return (
+    <div>
+    <Hello author="EU"
+     content ="lorem"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hello.jsx
@@ -0,0 +1,8 @@
+export function Hello (props){
+    return (
+        <div>
+      <p>{props.author}</p>
+      <strong>{props.content}</strong>
+      </div>
+    )
+}
